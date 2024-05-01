bb40fd8f6d2cf0f028fcfa25f28a0e3757ab0474
Daniel Birsan <75643437+danielbirsan@users.noreply.github.com>
2024-05-01 18:09:54 +0300

    Update TwoThreeHeap.cpp

--- Source/TwoThreeHeap.cpp
+++ Source/TwoThreeHeap.cpp
@@ -1,1 +1,91 @@
-#include "../Header/TwoThreeHeap.h"
+#include "heap23.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+/*
+ * 
+ * testari
+    Heap23<int> heap;
+   heap.insert(1, 11);
+    heap.insert(2, 21);
+    heap.print(cout);
+    cout << "end"<<endl;
+
+    heap.insert(3, 13);
+    heap.print(cout);
+    cout << "end"<<endl;
+
+    heap.insert(4, 14);
+
+    heap.print(cout);
+    cout << "end"<<endl;
+    heap.insert(5, 25);
+
+    heap.print(cout);
+    cout << "end"<<endl;
+    heap.insert(6, 1);
+
+    heap.print(cout);
+    cout << "end"<<endl;
+    heap.insert(7, 2);
+
+    heap.print(cout);
+    cout << "end"<<endl;
+    heap.insert(8, 3);
+
+    heap.print(cout);
+    cout << "end"<<endl;
+    heap.insert(9, 4);
+
+    heap.print(cout);
+    cout << "end"<<endl;
+    heap.insert(10, 5);
+
+    heap.print(cout);
+    cout << "end"<<endl;
+    heap.insert(11, 6);
+
+    heap.print(cout);
+    cout << "end"<<endl;
+    heap.insert(12, 7);
+
+    heap.print(cout);
+    cout << "end"<<endl;
+    heap.insert(13, 8);
+
+    heap.print(cout);
+    cout << "end"<<endl;
+    heap.insert(14, 9);
+    heap.insert(15, 10);
+    heap.insert(16, 11);
+    heap.insert(17, 12);
+    heap.insert(18, 13);
+    heap.insert(19, 14);
+    heap.insert(20, 15);
+    heap.insert(21, 16);
+    heap.insert(22, 17);
+    heap.insert(23, 18);
+    heap.insert(24, 19);
+    heap.insert(25, 20);
+    heap.insert(26, 21);
+
+
+
+
+    heap.print(cout);
+    cout << "end"<<endl;
+
+
+    cout << "Extracted min: " <<heap.extractMin()->value()<< endl;
+
+
+
+*/
+
+
+
+}
